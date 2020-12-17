0bf0339b34b3d820407d43cb4f73bc0f97320f64
jacob19891989 <52756596+jacob19891989@users.noreply.github.com>
2020-12-17 10:38:44 +0100

    Add files via upload

---
+++ convertminutesintoseconds.py
@@ -0,0 +1,11 @@
+#convert minutes into seconds
+
+def convert(minutes):
+        return minutes *60
+
+print(convert(30))
+print(convert(1))
+print(convert(2))
+print(convert(3))
+print(convert(4))
+print(convert(5))
