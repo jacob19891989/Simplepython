9dde3ed8cdb8895bee6b631053d143bf989f198f
jacob19891989 <52756596+jacob19891989@users.noreply.github.com>
2020-12-17 10:28:01 +0100

    Add files via upload

---
+++ shortcode.py
@@ -0,0 +1,11 @@
+#converthoursinto seconds
+
+def how_many_seconds(k):
+        return k*3600
+        
+print(how_many_seconds(20))
+
+def tri_area(base, height):
+        return(base*height)/2
+
+print(tri_area(2,4))
