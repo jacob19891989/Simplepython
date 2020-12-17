5a010886eaeaa877529f85eb30f755ff0018ed5b
jacob19891989 <52756596+jacob19891989@users.noreply.github.com>
2020-12-17 14:54:11 +0100

    Add files via upload

---
+++ winpercentage.py
@@ -0,0 +1,13 @@
+#win percentage
+
+def win_percentage(wins,losses):
+        return wins /(wins+losses)
+
+
+print(win_percentage(1,2))
+
+print(win_percentage(10,29))
+
+print(square_root(3,3))
+
+print(square_root(3,4))
