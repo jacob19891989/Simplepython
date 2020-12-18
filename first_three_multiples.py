da4f6f2c45983d0a55a03784d94ff46811bf468e
jacob19891989 <52756596+jacob19891989@users.noreply.github.com>
2020-12-18 14:29:12 +0100

    Add files via upload

---
+++ first_three_multiples.py
@@ -0,0 +1,14 @@
+#first three multiples
+
+
+def first_three_multiples(num):
+        print(num*1)
+        print(num*2)
+        print(num*3)
+        return num*3
+
+first_three_multiples(10)
+
+first_three_multiples(29)
+
+first_three_multiples(34)
