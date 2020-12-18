830dd357c20489ab775534793e1e698e021db786
jacob19891989 <52756596+jacob19891989@users.noreply.github.com>
2020-12-18 15:36:23 +0100

    Add files via upload

---
+++ string_concatenation.py
@@ -0,0 +1,12 @@
+#String concatenation
+
+first_name="Raul"
+last_name="Ortega"
+
+def account_generator(first_name, last_name):
+        account_name = first_name[0:3]+last_name[0:3]
+        
+new_account=account_generator("Raul","Ortega")
+
+
+
