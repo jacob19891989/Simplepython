d4f9ca945cec51f21c0a36181de7548c70d9851f
jacob19891989 <52756596+jacob19891989@users.noreply.github.com>
2020-12-18 15:08:51 +0100

    Add files via upload

---
+++ dogyears.py
@@ -0,0 +1,12 @@
+#It calculates the age of a person in dog years
+
+def dog_years(name,age)_
+        return "name, you are str(age) years old in dog years"
+        
+print(dog_years("Lola",16))
+print(dog_years("Baby",0))
+
+
+
+
+
