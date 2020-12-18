4a8f1d836fc7d894521af113c6f4a6a8d8a5a30e
jacob19891989 <52756596+jacob19891989@users.noreply.github.com>
2020-12-18 14:38:00 +0100

    Add files via upload

---
+++ tip.py
@@ -0,0 +1,9 @@
+#total and percentage. A function that returns the amount you should tip given a total and the percentage that you want to tip.
+
+def tip(total, percentage):
+        return (1/(percentage)) *total
+        
+
+
+print(tip(10,25))
+print(tip(20,234))
