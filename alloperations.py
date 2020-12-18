5541b4ebc6756df5198964716140ecfca6ccc1a7
jacob19891989 <52756596+jacob19891989@users.noreply.github.com>
2020-12-18 15:02:05 +0100

    Add files via upload

---
+++ alloperations.py
@@ -0,0 +1,13 @@
+#all basi arithmetic operations in one code
+
+def lots_of_math(a,b,c,d):
+        print(a+b)
+        print(c-d)
+        print((a+b)*(c-d))
+        return (a+b)*(c-d) %a
+        
+
+print(lots_of_math(1,2,3,4))
+print(lots_of_math(1,1,1,1))
+
+
