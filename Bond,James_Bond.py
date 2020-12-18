d5cfa0c183ad06c99c855140b565694c54c602f6
jacob19891989 <52756596+jacob19891989@users.noreply.github.com>
2020-12-18 14:54:59 +0100

    Add files via upload

---
+++ Bond,James_Bond.py
@@ -0,0 +1,11 @@
+#Bond, James Bond, a program that introduces a person first with his surname
+
+def introduction(first_name, last_name):
+        return last_name, first_name+''+last_name
+
+
+print(introduction("James", "Bond"))
+print(introduction("Jacob", "Orenday"))
+
+
+
